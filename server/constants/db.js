b55492271637e66dc2477b7837cc3ef66b6cdf08
Alan Elias <alan@elias.fyi>
2019-05-30 15:59:23 -0700

    node server init

---
+++ server/constants/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD
+};
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { Pool } from 'pg'
+import dbCredentials from './constants/db'
+
+// Setting up Express.js server
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const db = new Pool(dbCredentials);
+
+db.on('error', () => console.log('connection error'));
+
+db.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
+
+// Express route handlers
+app.get('/', (req, res) => {
+    res.send('Server is running..');
+});
