cc4428ca6fe269ee1afd4dfdb8d85e78abb93b6d
Alan Elias <alan@elias.fyi>
2019-05-30 18:18:31 -0700

    mapping docker server ports and listen to 3000

--- server/server.js
+++ server/server.js
@@ -21,3 +21,7 @@ db.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 app.get('/', (req, res) => {
     res.send('Server is running..')
 })
+
+app.listen(3000, err => {
+    console.log('Listening')
+});
